8e8e206006e72509f2b970b8775ec4c309430b1c
SeedPuller <seedpuller@gmail.com>
2018-09-10 00:52:34 +0430

    Installing simin-no

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo mkdir $SIMINP;
 sudo cp -r * $SIMINP/
 sudo cp simin /usr/bin/
 sudo chmod +x $USRSIMIN
+sudo cp simin-no /usr/bin/
+sudo chmod +x /usr/bin/simin-no
 sudo cp simin.desktop /usr/share/applications
-sudo rm -rf $SIMINP/simin.desktop $SIMINP/install.sh __pycache__ $SIMINP/simin
+sudo rm -rf $SIMINP/simin.desktop $SIMINP/install.sh __pycache__ $SIMINP/simin $SIMINP/simin-no
 sudo chown -R $(id -u):$(id -g) $SIMINP
